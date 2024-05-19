b6e54f3e891cfb4144ddd20d4fde462e5ec5a4a5
leghnimi <ghenimi-hichem@hotmail.com>
2024-05-19 15:55:49 +0100

    not found file and function in nextjs

--- src/app/products/[id]/reviews/[reviewId]/page.jsx
+++ src/app/products/[id]/reviews/[reviewId]/page.jsx
@@ -1,6 +1,9 @@
-
+import { notFound } from "next/navigation"
 export default function ProductReviews( {params}) {
-    console.log(params)
+
+if(parseInt(params.reviewId) > 1000) {
+  notFound()
+}
   return (
     <div>ProductReviews {params.reviewId} </div>
   )
